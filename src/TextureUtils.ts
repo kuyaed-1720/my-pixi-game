98c1eee2c5512dfdcd35c476481a513b80c2c153
kuyaed-1720 <kuya.ed20@gmail.com>
2026-02-07 06:48:52 +0800

    Add texture cropper for spritesheets

---
+++ src/TextureUtils.ts
@@ -0,0 +1,15 @@
+import * as PIXI from 'pixi.js';
+
+export function getFrame(
+    baseTexture: PIXI.Texture,
+    col: number,
+    row: number,
+    width: number,
+    height: number,
+): PIXI.Texture {
+    const frame = new PIXI.Rectangle(col * width, row * height, width, height);
+    return new PIXI.Texture({
+        source: baseTexture.source,
+        frame: frame
+    });
+}
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,15 @@
 import * as PIXI from 'pixi.js';
 import { Player } from './Player';
+import { getFrame } from './TextureUtils';
 
 async function init() {
     const app =  new PIXI.Application();
-
-    await app.init({
-        width: 800,
-        height: 600,
-    });
-
+    await app.init({ width: 800, height: 600 });
     document.body.appendChild(app.canvas);
 
-    const texture = await PIXI.Assets.load('/public/elf.png');
-    const hero = new Player(texture, 400, 300);
+    const entitySheet = await PIXI.Assets.load('/public/elf.png');
+    const heroTexture = getFrame(entitySheet, 0, 0, 16, 16);
+    const hero = new Player(heroTexture, 400, 300);
     app.stage.addChild(hero.sprite);
 
     app.ticker.add((time) => {
