a49b9c555a4634f9da8fb3f5e5e3d912d1b8708d
kuyaed-1720 <kuya.ed20@gmail.com>
2026-02-07 08:12:45 +0800

    Layout the game container

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,25 @@ import { getFrame } from './TextureUtils';
 import { DungeonMap } from './Map';
 
 async function init() {
+    // Get the game container
+    const container = document.getElementById('pixi-content');
+    if(!container) {
+        console.error("Game container 'pixi-content' not found!");
+        return;
+    }
+
+    // Initialize app
     const app =  new PIXI.Application();
     await app.init({
-        width: 400,
-        height: 300,
+        resizeTo: container,
         resolution: window.devicePixelRatio || 1,
         autoDensity: true,
     });
     PIXI.TextureSource.defaultOptions.scaleMode = 'nearest';
-    document.body.appendChild(app.canvas);
 
+    container.appendChild(app.canvas);
+
+    // Ready the textures
     const floorSheet = await PIXI.Assets.load('/public/grounds.png');
     const floorTexture = getFrame(floorSheet, 0, 0, 16, 16);
     const map = new DungeonMap(floorTexture, app.screen.width, app.screen.height);
@@ -21,7 +30,8 @@ async function init() {
     const entitySheet = await PIXI.Assets.load('/public/elf.png');
     const heroTexture = getFrame(entitySheet, 0, 0, 16, 16);
     const hero = new Player(heroTexture, 400, 400);
-    
+
+    // Add to canvas
     app.stage.addChild(map.floor);
     app.stage.addChild(hero.sprite);
 
