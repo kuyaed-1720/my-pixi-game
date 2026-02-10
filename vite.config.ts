2e7a08c6833700cba46ae3f6e385cb0df9417140
kuyaed-1720 <kuya.ed20@gmail.com>
2026-02-10 09:03:58 +0800

    Fix vite folder issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    base: '/my-pixi-game/',
+    base: '/my-pixi-game/dev',
     build: {
-        outDir: 'dist/dev',
+        outDir: 'dist',
         rollupOptions: {
             input: {
                 main: 'index.html',
