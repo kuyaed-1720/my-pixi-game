e6b215a9f8a4a971b1aa526a20be3483da996da9
kuyaed-1720 <kuya.ed20@gmail.com>
2026-02-10 08:24:21 +0800

    Add test server for combay-system branch at https://kuyaed-1720.github.io/my-pixi-game/dev/

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
     base: '/my-pixi-game/',
     build: {
-        outDir: 'dist',
+        outDir: 'dist/dev',
         rollupOptions: {
             input: {
                 main: 'index.html',
